c6aef878f52dc80407b2d9ae1e82b7f2e7d9b226
yambeni <mirriamyambeni1@gmail.com>
2023-03-24 16:58:47 +0200

    intro to c#

---
+++ lab1/Program.cs
@@ -0,0 +1,70 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to c#");
+
+// basics string variable
+string firstName = "Mirriam";
+string lastName = "Yambeni";
+Console.Write(firstName);
+Console.Write(" ");
+Console.Write(lastName);
+Console.WriteLine();
+
+Console.WriteLine("\nStrings & reassigning");
+Console.WriteLine("---------------------------");
+
+
+firstName = "Resten";
+lastName = "Madzalo";
+Console.WriteLine(firstName);
+Console.WriteLine(lastName);
+
+// int, float, decimal bool
+Console.WriteLine("\nIntegers, float, decimal, bolean");
+Console.WriteLine("-------------------------------------");
+
+
+int age = 21;
+float length = 294302f;
+decimal width = 2.0m;
+bool isRegistered = true;
+
+Console.WriteLine("age: "+ age);
+Console.WriteLine("length: "+ length);
+Console.WriteLine("width: "+ width);
+Console.WriteLine("is registered: "+ isRegistered);
+
+// var
+Console.WriteLine("\nImplicitly typed local variable");
+Console.WriteLine("-------------------------------------");
+
+var userName = "Olivia";
+var aged = 21;
+float height = 294302f;
+decimal breadth = 2.0m;
+bool isCold = true;
+
+Console.WriteLine("userName: "+ userName);
+Console.WriteLine("aged: "+ aged);
+Console.WriteLine("height: "+ height);
+Console.WriteLine("breadth: "+ breadth);
+Console.WriteLine("isCold: "+ isCold);
+
+Console.WriteLine("\nEscape sequence");
+Console.WriteLine("-------------------------------------");
+
+Console.WriteLine("Her name is \"Mirriam\"");
+
+
+Console.WriteLine("\nInterpolation, verbatim");
+Console.WriteLine("-------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
